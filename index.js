32410380e2a38cde2928b6a23a2bddd1e4821281
Md. Rabbinur Muktar <rabbinur.cse.bubt@gmail.com>
2022-11-15 18:17:00 +0600

    set env for password user not sending to github

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//mongodb
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pauaaue.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("car-wash server is running");
